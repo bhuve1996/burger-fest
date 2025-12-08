9b39728cbf0ca267c36fc4286d56d1f12d6df970
Bhuvesh Singla <bhuvesh.singla@akqa.com>
2025-12-08 16:20:54 +0530

    Change app.config.js to direct export (not function) to fix projectRoot

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -4,63 +4,59 @@ const { resolve } = require('path');
 // Load .env from root folder BEFORE anything else
 config({ path: resolve(__dirname, '../../.env') });
 
-// Use a function export to ensure Expo can detect project root
-module.exports = function () {
-  return {
-    expo: {
-      name: 'Burger Fest',
-      slug: 'burger-fest',
-      version: '1.0.0',
-      orientation: 'portrait',
-      icon: './assets/icon.png',
-      userInterfaceStyle: 'light',
-      splash: {
-        image: './assets/splash.png',
-        resizeMode: 'contain',
+// Export config directly (not as a function)
+module.exports = {
+  expo: {
+    name: 'Burger Fest',
+    slug: 'burger-fest',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.burgerfest.app',
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#ffffff',
       },
-      assetBundlePatterns: ['**/*'],
-      ios: {
-        supportsTablet: true,
-        bundleIdentifier: 'com.burgerfest.app',
-      },
-      android: {
-        adaptiveIcon: {
-          foregroundImage: './assets/adaptive-icon.png',
-          backgroundColor: '#ffffff',
-        },
-        package: 'com.burgerfest.app',
-      },
-      web: {
-        favicon: './assets/favicon.png',
-      },
-      scheme: 'burger-fest',
-      plugins: [
-        'expo-router',
-        'react-native-reanimated/plugin',
-        [
-          'expo-build-properties',
-          {
-            ios: {
-              useFrameworks: 'static',
-            },
-          },
-        ],
-      ],
-      extra: {
-        EXPO_PUBLIC_SUPABASE_URL:
-          process.env.EXPO_PUBLIC_SUPABASE_URL ||
-          process.env.SUPABASE_URL ||
-          '',
-        EXPO_PUBLIC_SUPABASE_ANON_KEY:
-          process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ||
-          process.env.SUPABASE_ANON_KEY ||
-          '',
-        EXPO_PUBLIC_API_URL:
-          process.env.EXPO_PUBLIC_API_URL ||
-          process.env.API_URL ||
-          'http://localhost:3000',
-      },
+      package: 'com.burgerfest.app',
     },
-  };
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    scheme: 'burger-fest',
+    plugins: [
+      'expo-router',
+      'react-native-reanimated/plugin',
+      [
+        'expo-build-properties',
+        {
+          ios: {
+            useFrameworks: 'static',
+          },
+        },
+      ],
+    ],
+    extra: {
+      EXPO_PUBLIC_SUPABASE_URL:
+        process.env.EXPO_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL || '',
+      EXPO_PUBLIC_SUPABASE_ANON_KEY:
+        process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ||
+        process.env.SUPABASE_ANON_KEY ||
+        '',
+      EXPO_PUBLIC_API_URL:
+        process.env.EXPO_PUBLIC_API_URL ||
+        process.env.API_URL ||
+        'http://localhost:3000',
+    },
+  },
 };
