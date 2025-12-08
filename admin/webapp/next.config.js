6e5063148186e9b75f4885e450d5a7eaaa69de87
Bhuvesh Singla <bhuvesh.singla@akqa.com>
2025-12-08 18:05:01 +0530

    Restructure admin: move Next.js project to admin/webapp/app

--- admin/webapp/next.config.js
+++ admin/webapp/next.config.js
@@ -2,12 +2,10 @@ const { config } = require('dotenv');
 const { resolve } = require('path');
 
 // Load .env from root folder
-config({ path: resolve(__dirname, '../../.env') });
+config({ path: resolve(__dirname, '../../../.env') });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Next.js App Router requires 'app' directory, but we're using 'webapp' for clarity
-  // We'll need to use pages directory or configure differently
   env: {
     // Expose NEXT_PUBLIC_* variables from root .env
     NEXT_PUBLIC_SUPABASE_URL:
