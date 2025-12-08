ec8e1269f9f20bae6d2805e153978cf3a64fb00e
Bhuvesh Singla <bhuvesh.singla@akqa.com>
2025-12-08 16:20:31 +0530

    Remove app.json, use only app.config.js to fix projectRoot error

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -49,7 +49,9 @@ module.exports = function () {
       ],
       extra: {
         EXPO_PUBLIC_SUPABASE_URL:
-          process.env.EXPO_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL || '',
+          process.env.EXPO_PUBLIC_SUPABASE_URL ||
+          process.env.SUPABASE_URL ||
+          '',
         EXPO_PUBLIC_SUPABASE_ANON_KEY:
           process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ||
           process.env.SUPABASE_ANON_KEY ||
