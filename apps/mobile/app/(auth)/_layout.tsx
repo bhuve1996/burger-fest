29d4c3addb577722fdcdb083a429a13d30e7fcfa
Bhuvesh Singla <bhuvesh.singla@akqa.com>
2025-12-08 17:45:23 +0530

    Replace all Stack/Tabs with Slot to isolate boolean type error

--- apps/mobile/app/(auth)/_layout.tsx
+++ apps/mobile/app/(auth)/_layout.tsx
@@ -1,9 +1,5 @@
-import { Stack } from 'expo-router';
+import { Slot } from 'expo-router';
 
 export default function AuthLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="login" options={{ headerShown: false }} />
-    </Stack>
-  );
+  return <Slot />;
 }
