b6f1bf64f70fe74f9dbcbe35ce1c0f80e65ef94a
Bhuvesh Singla <bhuvesh.singla@akqa.com>
2025-12-08 17:44:18 +0530

    Use Slot instead of Stack in root layout to bypass boolean type error

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,20 +1,5 @@
-import { Stack } from 'expo-router';
-import { useEffect } from 'react';
-import { LogBox } from 'react-native';
+import { Slot } from 'expo-router';
 
 export default function RootLayout() {
-  // useEffect(() => {
-  //   // Suppress known deprecation warning from Expo Router internals
-  //   // This will be fixed in future Expo Router updates
-  //   LogBox.ignoreLogs([
-  //     'props.pointerEvents is deprecated. Use style.pointerEvents',
-  //   ]);
-  // }, []);
-
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="(auth)" />
-    </Stack>
-  );
+  return <Slot />;
 }
