4cf5c6708b0516b56acfd0de3d8a30cc54c4b0cc
Bhuvesh Singla <bhuvesh.singla@akqa.com>
2025-12-08 17:37:35 +0530

    Remove all headerShown options to isolate boolean type error

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -3,18 +3,18 @@ import { useEffect } from 'react';
 import { LogBox } from 'react-native';
 
 export default function RootLayout() {
-  useEffect(() => {
-    // Suppress known deprecation warning from Expo Router internals
-    // This will be fixed in future Expo Router updates
-    LogBox.ignoreLogs([
-      'props.pointerEvents is deprecated. Use style.pointerEvents',
-    ]);
-  }, []);
+  // useEffect(() => {
+  //   // Suppress known deprecation warning from Expo Router internals
+  //   // This will be fixed in future Expo Router updates
+  //   LogBox.ignoreLogs([
+  //     'props.pointerEvents is deprecated. Use style.pointerEvents',
+  //   ]);
+  // }, []);
 
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="(auth)" />
     </Stack>
   );
 }
